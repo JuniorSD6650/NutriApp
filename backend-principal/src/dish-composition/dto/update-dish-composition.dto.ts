9a78cd439cedf2d7cd874aebb466169ca9648160
JuniorSD6650 <fancy6650@gmail.com>
2025-11-06 14:24:09 -0500

    Iniciando de 0

--- backend-principal/src/dish-composition/dto/update-dish-composition.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateDishCompositionDto } from './create-dish-composition.dto';
-
-export class UpdateDishCompositionDto extends PartialType(CreateDishCompositionDto) {}
